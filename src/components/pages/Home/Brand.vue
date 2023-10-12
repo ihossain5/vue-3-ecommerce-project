ba9a045c64d78a20656d822984d349fec136b32b
ihossain5 <ismail.hossain5169@gmail.com>
2023-10-12 17:25:18 +0600

    shop & brand dynamically loaded

---
+++ src/components/pages/Home/Brand.vue
@@ -0,0 +1,47 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const brands = ref({})
+    return {
+        brands
+    }
+  },
+  async created() {
+    await fetch("https://api.twowheelersbd.com/api/brands", {
+        method: 'GET' //optional
+    })
+      .then(async (response) => {
+        const data = await response.json()
+
+        this.brands = data.resutls.data
+      })
+      .catch((error) => {return error})
+  },
+}
+</script>
+
+<template>
+  <section class="our_brands_sc spy">
+        <div class="container-fluid container-xxxl">
+            <div class="sc_title_wrapper">
+                <h1 class="sc_title">Our Brands</h1>
+                <a href="#" class="br_5 bg_red btnp_1228 tbg_3 hover">
+                    <span class="fw_5 fs_14 lh_20 fc_white text-uppercase">view all</span>
+                </a>
+            </div>
+            <div class="row row-cols-3 row-cols-sm-4 row-cols-lg-5 row-cols-xl-6 gx-2 gy-3 gy-md-4 gx-md-5">
+                
+                <div class="row_item" v-for="brand in brands" :key="brand.id">
+                    <a href="single-brand.html"
+                        class="d-flex justify-content-center align-items-center brand_card bg_white br_5 bxy_gl bs_15 tbs_tb_3 hover">
+                        <img :src="brand.photo" alt="logo" class="w-100">
+                    </a>
+                </div>
+
+               
+            </div>
+        </div>
+    </section>
+</template>
---
+++ src/components/pages/Home/Shop.vue
@@ -0,0 +1,66 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const shops = ref({})
+    return {
+        shops
+    }
+  },
+  async created() {
+    await fetch("https://api.twowheelersbd.com/api/shops", {
+        method: 'GET' //optional
+    })
+      .then(async (response) => {
+        const data = await response.json()
+
+        this.shops = data.resutls.data
+      })
+      .catch((error) => {return error})
+  },
+}
+</script>
+
+<template>
+    <section class="our_shop_sc spy">
+        <div class="container-fluid container-xxxl">
+            <div class="sc_title_wrapper">
+                <h1 class="sc_title">our shop</h1>
+                <a href="#" class="br_5 bg_red btnp_1228 tbg_3 hover">
+                    <span class="fw_5 fs_14 lh_20 fc_white text-uppercase">view all</span>
+                </a>
+            </div>
+            <div class="row row-cols-2 row-cols-lg-3 g-2 g-sm-3 g-xl-5">
+                <div class="row_item" v-for="shop in shops" :key="shop.id">
+                    <div
+                        class="d-flex flex-wrap flex-lg-nowrap gap-2 gap-md-3 gap-lg-4 shop_card br_5 bxy_gl bs_15 tbs_tb_3 hover">
+                        <div>
+                            <div class="img_box">
+                                <img :src="shop.logo" alt="thumb" class="w-100">
+                            </div>
+                        </div>
+                        <div>
+                            <div
+                                class="h-100 d-flex flex-column justify-content-between align-items-center align-items-md-start gap-2">
+                                <div class="text-center text-md-start">
+                                    <h4 class="mb-md-2 fw_5 fs_20 lh_29">{{ shop.name }}</h4>
+                                    <div class="rate_wrapper">
+                                        <div class="rating rating_16" data-rateyo-read-only="true"></div>
+                                    </div>
+                                    <p class="pt-md-3 fs_15 lh_22 fc_gd">{{ shop.division }}</p>
+                                </div>
+                                <div>
+                                    <a href="#" class="d-inline-block br_5 bg_red btnp_1530 tbg_3 hover">
+                                        <span class="fw_5 fs_14 lh_20 fc_white text-uppercase">view all</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+</template>
