fe9096f420e68c8835a0720198d1ecbccc2bd796
ihossain5 <ismail.hossain5169@gmail.com>
2023-12-31 20:04:02 +0600

    issue fixed

--- src/views/OrderConfirmView.vue
+++ src/views/OrderConfirmView.vue
@@ -2,13 +2,13 @@
         <section class="confirm_order_page spb">
         <div class="container-fluid container-xxxl">
             <div class="d-flex flex-column align-items-center">
-                <img src="./assets/images/conf-order.png" alt="icon">
+                <!-- <img src="./assets/images/conf-order.png" alt="icon"> -->
                 <h1 class="smt_4">Your order has been Placed</h1>
                 <p class="mt-2 mt-md-3 fs_17 lh_25 fc_gd">Thank you for ordering from <span class="fc_black">Two Wheelers
                         BD.</span>
                 </p>
                 <div class="spt_4">
-                    <a href="my-orders.html" class="d-inline-block br_5 bg_red btnp_1228 tbg_3 hover">
+                    <a href="#" class="d-inline-block br_5 bg_red btnp_1228 tbg_3 hover">
                         <span class="fw_5 fs_14 lh_20 fc_white text-uppercase">View your order</span>
                     </a>
                 </div>
